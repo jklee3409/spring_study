4edfe97bb6580ee071f888c40e20bcfbd2bbaf2d
jklee3409 <jklee3409@khu.ac.kr>
2024-06-14 00:17:06 +0900

    Testcase Correction

--- src/test/java/hello/core/member/OrderServiceTest.java
+++ src/test/java/hello/core/member/OrderServiceTest.java
@@ -1,14 +1,23 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class OrderServiceTest {
-    MemberService memberService = new MemberServiceImpl();
-    OrderService orderService = new OrderServiceImpl();
+    MemberService memberService;
+    OrderService orderService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+        orderService = appConfig.orderService();
+    }
 
     @Test
     void createOrder() {
