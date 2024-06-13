4a7358bc2c9f40572079923f145eec3a4fd67e6c
jklee3409 <jklee3409@khu.ac.kr>
2024-06-13 23:47:31 +0900

    Member Order Service

---
+++ src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -0,0 +1,18 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy{
+
+    private int discountPrice = 10;
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP){
+            return price * discountPrice / 100;
+        } else {
+            return 0;
+        }
+    }
+}
