5082c4c94faa957fe1d93e53d2238dfc9e3c664c
jklee3409 <jklee3409@khu.ac.kr>
2024-06-14 11:04:24 +0900

    AppConfig Refactoring 2

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -3,6 +3,7 @@ package hello.core;
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
+import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
@@ -15,12 +16,12 @@ public class AppConfig {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private static MemoryMemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
-
     public OrderService orderService() {
         return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    private static MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public DiscountPolicy discountPolicy(){
