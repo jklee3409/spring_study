ebd92dac626533f063933b1b055aef8a51bf1e3e
jklee3409 <jklee3409@khu.ac.kr>
2024-10-04 11:28:25 +0900

    Annotation Add

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -18,6 +18,7 @@ public class OrderApp {
         //MemberService memberService = appConfig.memberService();
         //OrderService orderService = appConfig.orderService();
 
+        // ApplicationContext : 스프링 컨테이너
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
         MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
