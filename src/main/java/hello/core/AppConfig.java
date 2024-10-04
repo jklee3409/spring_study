dd44c10874ede92b48bf41db96afba35f083a72e
jklee3409 <jklee3409@khu.ac.kr>
2024-10-04 11:32:45 +0900

    Annotation Add

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -12,8 +12,13 @@ import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+
+// 스프링은 @Configuration이 붙은 AppConfig를 설정 정보로 사용한다.
 @Configuration
 public class AppConfig {
+
+    // @Bean이 적힌 메서드를 모두 호출하여 반환된 객체를 스프링 컨테이너에 등록.
+    // 스프링 빈은 @Bean이 붙은 메서드의 명을 스프링 빈의 이름으로 사용한다.
 
     @Bean
     public MemberService memberService(){
