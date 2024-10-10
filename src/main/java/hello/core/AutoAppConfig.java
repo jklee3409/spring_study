012dff0a174d4bbe60a62c198c51683fe5e02408
jklee3409 <jklee3409@khu.ac.kr>
2024-10-10 22:17:37 +0900

    ComponentScan Start Position

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다.
 @ComponentScan(
+        // basePackages = "hello.core.member",
+        // basePackageClasses = AutoAppConfig.class,
         // AppConfig의 bean도 같이 자동으로 등록되는 것을 방지하기 위해.
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
