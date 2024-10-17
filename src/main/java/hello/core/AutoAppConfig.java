126fdab7ebaa0331f7ccda2d798bb0a48a68a3a7
jklee3409 <jklee3409@khu.ac.kr>
2024-10-17 23:24:55 +0900

    refactor

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 // @Component 애노테이션이 붙은 클래스를 스캔해서 스프링 빈으로 등록한다.
 @ComponentScan(
+        // refactor
         // basePackages = "hello.core.member",
         // basePackageClasses = AutoAppConfig.class,
         // AppConfig의 bean도 같이 자동으로 등록되는 것을 방지하기 위해.
